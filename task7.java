13a2779f9c0370812b83aa2ecef92a7d14cd4694
stameying <stameying@dhcp-164-107-134-97.osuwireless.ohio-state.edu>
2013-08-06 14:01:20 -0400

    lots update

--- task7.java
+++ task7.java
@@ -6,9 +6,12 @@ import java.util.Map;
 import redis.clients.jedis.Jedis;
 
 public class task7 {
+	private static Map<Integer, Integer> library;
+	private Jedis jedis;
 	
 	public task7(){
-		
+		library = new HashMap<Integer,Integer>();
+		Jedis jedis = new Jedis("localhost",6379);
 	}
 	
 	public static void main(String[] agrs) throws IOException
@@ -16,11 +19,11 @@ public class task7 {
 		Jedis jedis = new Jedis("localhost",6379);
 		task7 test = new task7();
 		InputReader2 ir = new InputReader2();
-		ir.readFile("E-mail.txt");
+		ir.readFile("E-mail6.txt");
 		ArrayList<String> content = ir.getContentList();
 		ArrayList<String> title = ir.getTitles();
 		IndexPath indexPath = new IndexPath();
-		IndexParagraph getParagraph = new IndexParagraph();
+		IndexParagraph4 getParagraph = new IndexParagraph4();
 		
 		normalizeAgent normalAgent = new normalizeAgent();
 		
@@ -28,6 +31,7 @@ public class task7 {
 	
 		long startTime=System.currentTimeMillis();  
 		
+		//System.out.println(content.get(0));
 		for (int i = 0; i < content.size() ; i++)
 		{
 			String mailIndex = title.get(i);
@@ -35,17 +39,73 @@ public class task7 {
 			ArrayList<Integer> result = getParagraph.indexString(i,mailContent,graph);
 			Map<Integer,String> library = new HashMap<Integer,String>();
 		    indexPath.getPath(i ,result);
-			
 		}
+		long endTime=System.currentTimeMillis(); 
+		System.out.println((endTime-startTime) + " ms");
+		
 		//indexPath.printPath();
 		//graph.printGraph();
 		//graph.show_key_nodes();
+		startTime=System.currentTimeMillis();  
 		graph.weightGraph1();
+		endTime=System.currentTimeMillis(); 
+		//System.out.println("Weight1:" + (endTime-startTime) + " ms");
+		
+		startTime=System.currentTimeMillis();  
 		graph.weightGraph2();
+		endTime=System.currentTimeMillis(); 
+		//System.out.println("Weight2:" + (endTime-startTime) + " ms");
+		
+		startTime=System.currentTimeMillis();  
 		graph.weightGraph3();
-		getParagraph.printNodeList();
+		endTime=System.currentTimeMillis(); 
+		//System.out.println("Weight3:" + (endTime-startTime) + " ms");
+		
+		
+		//getParagraph.printNodeList();
 		//getParagraph.printLibrary();
+	
+		//graph.test2();
+		
+		library = getParagraph.getLibrary();
+		
+		test.run(content,getParagraph);
+		test.run(content,getParagraph);
+		test.run(content,getParagraph);
+		test.run(content,getParagraph);
+		test.run(content,getParagraph);
+		test.run(content,getParagraph);
+	}
+	
+	public void run(ArrayList<String> incontent, IndexParagraph4 inpara) throws IOException
+	{	
+		long startTime=System.currentTimeMillis();  
+		for (int i = 0 ; i < incontent.size() ; i++)
+		{
+		ArrayList<Integer> result = inpara.indexString2(i, incontent.get(i));
+	
+		int count = 0;
+		while (count < result.size())
+		{
+			int hash = result.get(count);
+			if (!checkHash(hash))
+			{
+				String paragraph = jedis.get(result.get(count)+"");
+				inpara.addToLibrary(paragraph,0);
+			}
+			count++;
+		}
+		}
+		
 		long endTime=System.currentTimeMillis(); 
 		System.out.println((endTime-startTime) + " ms");
 	}
+
+	public boolean checkHash(int hash)
+	{
+	 return library.containsKey(hash);
+	}
+
 }
+
+
